969a729f21f5d432365a53363f144b145e455cc4
Ahmed <mabrook.amed@gmail.com>
2023-01-19 16:40:21 +0200

    updating visualization script

---
+++ src/visualization/visualize.py
@@ -0,0 +1,21 @@
+# This the script to visualize the model output either in training or testing 
+
+from matplotlib.colors import ListedColormap
+
+
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() -10, stop = X_set[:, 1].max() + 10, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+
+
+plt.title('title')
+plt.xlabel('x lable')
+plt.ylabel('y label')
+plt.legend()
+plt.show()
