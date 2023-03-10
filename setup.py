89fafaaa9dbcb86016bafa3556dbd9631a9762c0
Ahmed <mabrook.amed@gmail.com>
2023-03-10 10:25:00 +0200

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 def get_install_requirements():    
 
@@ -10,12 +10,11 @@ def get_install_requirements():
 
 setup(
     name='AIEMS',
-    packages=find_packages(),
     version='0.1.0',
     description='Ai-based Energy Managment System',
     author='Ahmed Mabrouk',
     author_email='mabrook.amed@gmail.com',
     license='CC0 1.0 Universal',
-    packages=setuptools.find_namespace_packages(include=['src.features','src.visualization'] ),
+    packages=setuptools.find_namespace_packages(include=['src', 'src.models','src.features', 'src.data'] ),
     install_requires= get_install_requirements()
 )
